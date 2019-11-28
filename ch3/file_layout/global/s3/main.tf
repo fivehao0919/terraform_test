f6a46b5c0723aa857143c54685843a0c62cebf00
wuhao4 <wuhao4@100tal.com>
2019-11-28 15:26:51 +0800

    add file layout

--- ch3/file_layout/global/s3/main.tf
+++ ch3/file_layout/global/s3/main.tf
@@ -39,16 +39,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
 
 terraform {
   backend "s3" {
-    key            = "global/s3/terraform.tfstate"
+    key = "workspaces-example/terraform.tfstate"
   }
 }
 
-output "s3_bucket_arn" {
-  value       = aws_s3_bucket.terraform_state.arn
-  description = "The ARN of the S3 bucket"
-}
-
-output "dynamodb_table_name" {
-  value       = aws_dynamodb_table.terraform_locks.name
-  description = "The name of the DynamoDB table"
-}
